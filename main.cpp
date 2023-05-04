ac8c76df7b8925bb126f0ed9695f75453b157f94
David Ash <davidjash2@gmail.com>
2023-05-04 19:00:14 -0400

    Added ability to read a board file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
+#include <fstream>
 #include <iostream>
 #include <vector>
+#include <string>
 using std::vector;
 using std::cout;
+using std::ifstream;
+using std::string;
+
+void ReadBoardFile(string fileName) {
+  ifstream inputFile;
+  inputFile.open(fileName);
+  if (inputFile) {
+    string line;
+    while(getline(inputFile, line)){
+      cout << line << "\n";
+    }
+  }
+}
 
 void PrintBoard(vector<vector<int>> board) {
   for (vector<int> r: board) {
@@ -13,10 +28,11 @@ void PrintBoard(vector<vector<int>> board) {
 }
 
 int main() {
-  vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
-                               {0, 1, 0, 0, 0, 0},
-                               {0, 1, 0, 0, 0, 0},
-                               {0, 1, 0, 0, 0, 0},
-                               {0, 0, 0, 0, 1, 0}};
-  PrintBoard(board);
+  ReadBoardFile("1.board");
+  // vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
+  //                              {0, 1, 0, 0, 0, 0},
+  //                              {0, 1, 0, 0, 0, 0},
+  //                              {0, 1, 0, 0, 0, 0},
+  //                              {0, 0, 0, 0, 1, 0}};
+  // PrintBoard(board);
 }
