726925486d0231fecae00c59231a6bbd44017463
David Ash <davidjash2@gmail.com>
2023-05-03 22:47:06 -0400

    Added abiltity to pring boared

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 using std::vector;
 using std::cout;
 
+void PrintBoard(vector<vector<int>> board) {
+  for (vector<int> r: board) {
+    for (int c: r) {
+      cout << c;
+    }
+    cout << "\n";
+  }
+}
 
 int main() {
   vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
@@ -10,5 +18,5 @@ int main() {
                                {0, 1, 0, 0, 0, 0},
                                {0, 1, 0, 0, 0, 0},
                                {0, 0, 0, 0, 1, 0}};
-  cout << "hello\n";
+  PrintBoard(board);
 }
