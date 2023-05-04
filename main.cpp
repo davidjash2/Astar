cac854068bfbc26ac77f20b1a3a052533020e8f3
David Ash <davidjash2@gmail.com>
2023-05-04 19:27:53 -0400

    Added ability to parse a board

--- main.cpp
+++ main.cpp
@@ -2,18 +2,35 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 using std::vector;
 using std::cout;
 using std::ifstream;
 using std::string;
+using std::istringstream;
+
+vector<int> ParseLine(string line) {
+  vector<int> parsedLine = {};
+  istringstream my_stream(line);
+
+  char c;
+  int n;
+
+  while (my_stream >> n >> c) {
+    parsedLine.push_back(n);
+  }
+  return parsedLine;
+} 
 
 void ReadBoardFile(string fileName) {
   ifstream inputFile;
   inputFile.open(fileName);
+  vector<vector<int>> board = {};
   if (inputFile) {
     string line;
     while(getline(inputFile, line)){
-      cout << line << "\n";
+      vector<int> pl = ParseLine(line);
+      board.push_back(pl);
     }
   }
 }
@@ -27,8 +44,11 @@ void PrintBoard(vector<vector<int>> board) {
   }
 }
 
+#include "test.cpp"
+
 int main() {
   ReadBoardFile("1.board");
+  TestParseLine();
   // vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
   //                              {0, 1, 0, 0, 0, 0},
   //                              {0, 1, 0, 0, 0, 0},
